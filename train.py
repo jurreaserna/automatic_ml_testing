9d3da20e0a17323fb78d719ac03d97276b203656
urrej008 <julian.a.urrea.-nd@disney.com>
2023-02-01 17:07:30 -0500

    trained a new model

--- train.py
+++ train.py
@@ -3,17 +3,16 @@ from sklearn.linear_model import LinearRegression
 import pickle
 import os
 
-X, y = make_regression(10000,n_features = 10)
+X, y = make_regression(10000, n_features=11)
 
 # Train a model
 reg = LinearRegression().fit(X, y.ravel())
 # Print out training r2
-print(reg.score(X,y.ravel() ))
+print(reg.score(X, y.ravel()))
 
 # Write the model to a file
 if not os.path.isdir("models/"):
     os.mkdir("models")
 
-filename = 'models/model.pkl'
-pickle.dump(reg, open(filename, 'wb'))
-
+filename = "models/model.pkl"
+pickle.dump(reg, open(filename, "wb"))
